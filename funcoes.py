f6fcab7105e55994573efb1aaf9c36095b16d9e1
Gabriel Mariano <gabrielmf6@al.insper.edu.br>
2025-04-28 07:54:50 -0300

    finish him

--- funcoes.py
+++ funcoes.py
@@ -100,3 +100,12 @@ def calcula_pontos_quina(lista):
             return 50
     return 0
 
+def calcula_pontos_regra_avancada(lista):
+    return {
+        'cinco_iguais': calcula_pontos_quina(lista),
+        'full_house': calcula_pontos_full_house(lista),
+        'quadra': calcula_pontos_quadra(lista),
+        'sem_combinacao': calcula_pontos_soma(lista),
+        'sequencia_alta': calcula_pontos_sequencia_alta(lista),
+        'sequencia_baixa': calcula_pontos_sequencia_baixa(lista)
+    }
